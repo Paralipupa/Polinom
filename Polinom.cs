ce1c4c05cc3df90dd85118971f2dea5c195bd46b
Paralipupa <a917kk@mail.ru>
2021-03-23 23:22:24 +0800

    Вычисление полинома

---
+++ Polinom.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinom
+{
+    class Polinom
+    {
+        public string Name { get; set; }
+        public Dictionary<int, float> Items = new Dictionary<int, float>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pol1"></param>
+        /// <param name="pol2"></param>
+        /// <returns></returns>
+        static public Polinom operator +(Polinom pol1, Polinom pol2)
+        {
+            Polinom polRes = new Polinom();
+            foreach (KeyValuePair<int, float> item in pol1.Items)
+            {
+                try
+                {
+                    polRes.Items.Add(item.Key, item.Value);
+
+                    if (pol2.Items.ContainsKey(item.Key))
+                    {
+                        polRes.Items[item.Key] += pol2.Items[item.Key];
+                        pol2.Items.Remove(item.Key);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Source}-{ex.Message}");
+                }
+            }
+
+            try
+            {
+                foreach (KeyValuePair<int, float> item in pol2.Items)
+                {
+                    polRes.Items.Add(item.Key, item.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Source}-{ex.Message}");
+            }
+
+            return polRes;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pol1"></param>
+        /// <param name="pol2"></param>
+        /// <returns></returns>
+        static public Polinom operator -(Polinom pol1, Polinom pol2)
+        {
+            Polinom polRes = new Polinom();
+            foreach (KeyValuePair<int, float> item in pol1.Items)
+            {
+                try
+                {
+                    polRes.Items.Add(item.Key, item.Value);
+
+                    if (pol2.Items.ContainsKey(item.Key))
+                    {
+                        polRes.Items[item.Key] -= pol2.Items[item.Key];
+                        if (polRes.Items[item.Key] == 0)
+                        {
+                            polRes.Items.Remove(item.Key);
+                        }
+                        pol2.Items.Remove(item.Key);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Source}-{ex.Message}");
+                }
+            }
+
+            try
+            {
+                foreach (KeyValuePair<int, float> item in pol2.Items)
+                {
+                    polRes.Items.Add(item.Key, -item.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Source}-{ex.Message}");
+            }
+
+            return polRes;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pol1"></param>
+        /// <param name="pol2"></param>
+        /// <returns></returns>
+        static public Polinom operator *(Polinom pol1, Polinom pol2)
+        {
+            Polinom polRes = new Polinom();
+            foreach (KeyValuePair<int, float> item1 in pol1.Items)
+            {
+                try
+                {
+                    foreach (KeyValuePair<int, float> item2 in pol2.Items)
+                    {
+                        int rank = item1.Key + item2.Key;
+                        float koef = item1.Value * item2.Value;
+
+                        if (polRes.Items.ContainsKey(rank) == false)
+                        {
+                            polRes.Items.Add(rank, koef);
+                        }
+                        else
+                        {
+                            polRes.Items[rank] += koef;
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Source}-{ex.Message}");
+                }
+            }
+
+            return polRes;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pol1"></param>
+        /// <param name="pol2"></param>
+        /// <returns></returns>
+        static public Polinom operator /(Polinom pol1, Polinom pol2)
+        {
+            Polinom polRes = new Polinom();
+            
+            if (pol1.Items.Count == 0)
+            {
+                return polRes;
+            }
+
+            KeyValuePair<int, float> firstPol1 = pol1.Items.First();
+            KeyValuePair<int, float> firstPol2 = pol2.Items.First();
+
+            int rankOff = firstPol1.Key - firstPol2.Key;
+            float koefOff = firstPol1.Value / firstPol2.Value;
+
+            if (rankOff >= 0)
+            {
+                polRes.Items.Add(rankOff, koefOff);
+
+                Polinom pol4 = pol2 * polRes;
+                Polinom pol5 = pol1 - pol4;
+
+                Polinom pol6 = pol5 / pol2;
+
+                polRes += pol6;
+            }
+            else
+            {
+                polRes = pol2;
+            }
+
+            return polRes;
+
+        }
+
+        static public Polinom Sort(Polinom polinom)
+        {
+            var items = polinom.Items.OrderByDescending(p => p.Key);
+
+            Polinom polSort = new Polinom();
+            foreach (KeyValuePair<int, float> keyVal in items)
+            {
+                polSort.Items.Add(keyVal.Key, keyVal.Value);
+            }
+
+            return polSort;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("polinom.exe <input.txt> <output.txt> ");
+                Environment.Exit(0);
+            }
+
+            string pathInput = args[0];
+            string pathOutput = args[1];
+
+            if (File.Exists(pathInput) == false)
+            {
+                Console.WriteLine($"File not exist {pathInput}");
+                Environment.Exit(0);
+            }
+
+            List<Polinom> items = InputData(pathInput);
+            if (items.Count >= 2)
+            {
+                Polinom polA = items[0];
+                Polinom polB = items[1];
+
+                List<Polinom> outItems = new List<Polinom>();
+
+                Polinom polC = Polinom.Sort(polA * polB);
+
+                Polinom polD = Polinom.Sort(polC / polB);
+                
+                Polinom polE = Polinom.Sort(polC / polA);
+
+                polA.Name = "a";
+                polB.Name = "b";
+                polC.Name = "c";
+                polD.Name = "d";
+                polE.Name = "e";
+
+                outItems.Add(polA);
+                outItems.Add(polB);
+                outItems.Add(polC);
+                outItems.Add(polD);
+                outItems.Add(polE);
+
+                OutputData(pathOutput, outItems);
+            }
+        }
+
+        static private List<Polinom> InputData(string path)
+        {
+            bool bFirst = true;
+            Polinom pol1 = new Polinom();
+            Polinom pol2 = new Polinom();
+
+            using (StreamReader st = new StreamReader(path))
+            {
+                string line;
+                while ((line = st.ReadLine()) != null)
+                {
+                    if ((string.IsNullOrWhiteSpace(line)) && pol1.Items.Count != 0)
+                    {
+                        bFirst = false;
+                    }
+                    else
+                    {
+                        string[] str = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (str.Length == 2)
+                        {
+
+                            try
+                            {
+                                int rank = Int32.Parse(str[1]);
+                                float koef = float.Parse(str[0]);
+                                if (bFirst)
+                                {
+                                    if (pol1.Items.ContainsKey(rank) == false)
+                                    {
+                                        pol1.Items.Add(rank, koef);
+                                    }
+                                    else
+                                    {
+                                        pol1.Items[rank] += koef;
+                                    }
+
+                                }
+                                else
+                                {
+                                    if (pol2.Items.ContainsKey(rank) == false)
+                                    {
+                                        pol2.Items.Add(rank, koef);
+                                    }
+                                    else
+                                    {
+                                        pol2.Items[rank] += koef;
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"{ex.Source}- {ex.Message}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            Polinom pola = Polinom.Sort(pol1);
+            Polinom polb = Polinom.Sort(pol2);
+
+            return new List<Polinom> { pola, polb };
+        }
+
+        static private void OutputData(string path, List<Polinom> items)
+        {
+            using (StreamWriter st = new StreamWriter(path))
+            {
+                foreach (Polinom polinom in items)
+                {
+                    st.WriteLine($"{polinom.Name}");
+                    foreach (KeyValuePair<int, float> keyVal in polinom.Items)
+                    {
+                        st.WriteLine($"{keyVal.Value} {keyVal.Key}");
+                    }
+                    st.WriteLine("");
+                }
+            }
+        }
+    }
+}
